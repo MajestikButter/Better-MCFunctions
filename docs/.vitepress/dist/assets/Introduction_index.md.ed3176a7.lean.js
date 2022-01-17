1b25ff98bb2ebb54545e7c9346f27c9ab3a7258c
MajestikButter <yonnicaminas@gmail.com>
2022-01-16 22:23:15 -0500

    docs fixed?

---
+++ docs/.vitepress/dist/assets/Introduction_index.md.ed3176a7.lean.js
@@ -0,0 +1,1 @@
+import{_ as t,c as e,o}from"./app.13718577.js";const l='{"title":"Introduction","description":"","frontmatter":{"title":"Introduction"},"headers":[],"relativePath":"Introduction/index.md","lastUpdated":1642357051800}',a={};function n(r,c,s,d,i,p){return o(),e("div")}var f=t(a,[["render",n]]);export{l as __pageData,f as default};
--- docs/index.md
+++ docs/index.md
@@ -1,3 +1,3 @@
-<font size="10">Welcome to this terrible documentation page!</font>
+<!-- <font size="10">Welcome to this terrible documentation page!</font> -->
 
-<font size="10">[Get started here](./Introduction/What)</font>
+<!-- <font size="10">[Get started here](./Introduction/What)</font> -->
